ad223df3ceeeb1f4346278efa526e99846d7d80e
Jon Baker <jbaker@wgm.us>
2017-04-28 15:57:30 -0500

    Adding orchestra testbench for testing

--- tests/SignedS3FilesystemAdapterTest.php
+++ tests/SignedS3FilesystemAdapterTest.php
@@ -1,6 +1,37 @@
 <?php 
 
+use Orchestra\Testbench\BrowserKit\TestCase;
+
 class SignedS3FilesystemAdapterTest extends TestCase {
+
+	protected function getPackageProviders($app)
+	{
+		return [
+			'jdavidbakr\SignedS3Filesystem\SignedS3FilesystemServiceProvider',
+			'Aws\Laravel\AwsServiceProvider',
+		];
+	}
+
+	public function getPackageAliases($app)
+	{
+		return [
+			'AWS'=>Aws\Laravel\AwsFacade::class
+		];
+	}
+
+	public function getEnvironmentSetUp($app)
+	{
+		$app['config']->set('filesystems.disks.s3-signed',[
+			'driver' => 's3-signed',
+			'key'    => 'your-key',
+			'secret' => 'your-secret',
+			'region' => 'your-region',
+			'bucket' => 'your-bucket',
+			'options' => [
+				'expiration'=>'time-to-expire-urls-in-seconds',
+			],
+		]);
+	}
 
 	/**
 	 * @test
